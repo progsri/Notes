8532382960801ec30adb4cfa8b9e3e80ae10cd1b
srikanth <machine.progsri@gmail.com>
2019-05-10 21:49:33 -0500

    get list of iles

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+
+const fs = require('fs');
+
+dir = "/home/progsri/ProgSri/CODE/Notes/Notes"
+
+
+var getFiles = function (path) {
+  let list = []
+  let files = fs.readdirSync(path);
+  files.forEach(element => {
+    let tmp_path = path + "/" + element
+    if (fs.lstatSync(tmp_path).isDirectory()) {
+      list.push(getFiles(tmp_path))
+    } else {
+      list.push(tmp_path)
+    }
+  });
+
+  return list
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.send(getFiles(dir))
 });
 
 module.exports = router;
