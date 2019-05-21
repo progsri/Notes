cbd7c8bf14d7269f26578134a2ce565c4405289e
Srikanth Lankapalli <machine.progsri@gmail.com>
2019-05-21 13:14:58 -0500

    Update constants.js

--- app/routes/constants.js
+++ app/routes/constants.js
@@ -1,10 +1,19 @@
-const PATH = "path"
-const RESOURCE = "resource"
-const STATUS = "status"
-const TITLE = "title"
-const TAG = "tag"
-const TOPIC = "topic"
+const PATH = "path";
+const RESOURCE = "resource";
+const STATUS = "status";
+const TITLE = "title";
+const TAGS = "tags";
+const TOPIC = "topic";
+const NOTES = "notes";
+const HASH = "hash";
 
-export {
-    PATH, RESOURCE, STATUS, TITLE, TAG, TOPIC
-}
+module.exports = {
+  PATH,
+  RESOURCE,
+  STATUS,
+  TITLE,
+  TAGS,
+  TOPIC,
+  NOTES,
+  HASH
+};
