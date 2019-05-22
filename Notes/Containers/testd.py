dc940f32110daf4d98487a0980efd8d32c24d8bf
srikanth <machine.progsri@gmail.com>
2019-05-22 18:45:36 -0500

    renamed to content

--- Notes/Containers/testd.py
+++
@@ -1,5 +0,0 @@
-import time
-
-while True:
-    print("Running process")
-    time.sleep(60)
--- Notes/Template/dependencies.js
+++
@@ -1,7 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/prism.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/plugins/line-numbers/prism-line-numbers.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/plugins/command-line/prism-command-line.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/components/prism-java.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/plugins/normalize-whitespace/prism-normalize-whitespace.min.js"></script>
