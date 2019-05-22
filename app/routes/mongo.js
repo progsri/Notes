a6bad9784b869f4efc4bd0d8dbe1e173c91aa823
Srikanth Lankapalli <machine.progsri@gmail.com>
2019-05-22 14:23:40 -0500

    Update mongo.js

--- app/routes/mongo.js
+++ app/routes/mongo.js
@@ -1,45 +1,94 @@
 const util = require("util");
 const assert = require("assert");
 const MongoClient = require("mongodb").MongoClient;
+const constants = require("./constants.js");
 
 // Connection URL
-const url = "mongodb://10.220.118.209:27017";
+const url = constants.MONGOURL;
 
 // Database Name
-const dbName = "Ninjashore";
+const dbName = constants.NINJASHORE;
 
 // Create a new MongoClient
 const client = new MongoClient(url);
-
-//Collection
-const collection = "Notes";
 
 // Use connect method to connect to the Server
 client.connect(function(err) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
   const db = client.db("Notes");
-  client.close();
+  //client.close();
 });
 
-function getdata(data) {
-  var selectPromise = () => {
-    return new Promise((resolve, reject) => {
-      client.connect(function(err) {
-        const db = client.db(dbName);
-        const collection = db.collection("Notes");
-        // Find some documents
-        collection.find({}).toArray(function(err, records) {
-          assert.equal(err, null);
-          console.log("Found the following records");
-          console.log(records);
-          resolve(records);
-        });
+var getRecords = function() {
+  return new Promise((resolve, reject) => {
+    client.connect(function(err) {
+      const db = client.db(dbName);
+      const collection = db.collection("Notes");
+      // Find some documents
+      collection.find({}).toArray(function(err, records) {
+        assert.equal(err, null);
+        //console.log("Found the following records");
+        console.log(records);
+        resolve(records);
+        //client.close();
       });
     });
-  };
+  });
+};
 
-  return selectPromise;
+var insertRecords = function(records) {
+  if (records.length > 0) {
+    client.connect(function(err) {
+      const db = client.db(dbName);
+      const collection = db.collection("Notes");
+      collection.insertMany(records, function(err, r) {
+        assert.equal(err, null);
+        console.log("Number of records inserted " + r.insertedCount);
+        //client.close();
+      });
+    });
+  }
+};
+
+function updateRecords(
+  path,
+  resource,
+  status,
+  title,
+  tags,
+  topic,
+  hash,
+  updatedon,
+  id
+) {
+  client.connect(function(err) {
+    const db = client.db(dbName);
+    const collection = db.collection("Notes");
+    let filter = { _id: id };
+    let operation = {
+      $set: {
+        path: path,
+        resource: resource,
+        status: status,
+        title: title,
+        tags: tags,
+        topic: topic,
+        hash: hash,
+        updatedon: updatedon
+      }
+    };
+    collection.updateMany(filter, operation, { upsert: true }, function(
+      err,
+      r
+    ) {
+      assert.equal(err, null);
+      console.log("after Updated " + util.inspect(r.result));
+      //client.close();
+    });
+  });
 }
 
-module.exports.getdata = getdata();
+exports.getRecords = getRecords;
+exports.insertRecords = insertRecords;
+exports.updateRecords = updateRecords;
