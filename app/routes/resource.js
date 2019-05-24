9655d4af53dd16ea4a369150166d790f37d09726
srikanth <machine.progsri@gmail.com>
2019-05-23 22:55:11 -0500

    completed dynamic routing

--- app/routes/resource.js
+++ app/routes/resource.js
@@ -2,12 +2,13 @@ var express = require("express");
 var router = express.Router();
 const fs = require("fs");
 const util = require('util')
+const constants = require('./constants')
+const process = require("process");
 
 router.get("/*", function (req, res, next) {
 
-  console.log('req ' + util.inspect(req));
-
-  path = "/home/progsri/Desktop/Notes/content/JavaScript_Language/ext.html";
+  path = process.cwd().replace("/app", "") + req.originalUrl.replace("/" + constants.RESOURCE, "")
+  console.log('get contents of ' + path)
   fs.readFile(path, 'utf8', function (err, contents) {
     res.render("content", {
       content: contents
