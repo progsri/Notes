9b8b40e6da73a8b86e8958173f3caf8e1ef14433
srikanth <machine.progsri@gmail.com>
2019-05-23 23:05:59 -0500

    adding backlog

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,12 +10,26 @@ router.get("/", function (req, res, next) {
     .then(data => {
       let ui = [];
       for (index in data) {
+        let color = "";
+        let status = "";
+        if (data[index][constants.STATUS] != undefined) {
+          if (data[index][constants.STATUS].includes("ongoing")) {
+            color = "badge badge-warning";
+            status = data[index][constants.STATUS];
+          }
+        } else {
+          color = "badge badge-primary";
+          status = constants.STATUS_BACKLOG;
+        }
+
         ui.push({
           path:
             constants.RESOURCE + "/" +
             data[index][constants.PATH].replace("../", "").replace("../", "") + "/" +
             data[index][constants.RESOURCE],
-          resource: data[index][constants.RESOURCE].replace(".html", "")
+          resource: data[index][constants.RESOURCE].replace(".html", "") + "   ",
+          status: status,
+          color: color
         });
       }
 
