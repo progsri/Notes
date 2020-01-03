6d57b9c08da675a53fecff324e78f21db647f89f
Srikanth Lankapalli <machine.progsri@gmail.com>
2020-01-03 11:35:00 -0600

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -37,7 +37,7 @@ router.get("/", function(req, res, next) {
             }
           }
           
-          tags =  data[index][constants.PATH].replace(constants.BASEPATH, "").replace(/\//g,' - ');
+          tags =  data[index][constants.PATH].replace(constants.BASEPATH + "/", "").replace(/\//g,' - ');
 
           ui.push({
             path:
