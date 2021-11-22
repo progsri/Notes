056c05dc6bdf986d460023101861156e12f7ce40
srikanth <machine.progsri@gmail.com>
2021-11-21 19:10:32 -0600

    editor

--- app_render/routes/editor.js
+++ app_render/routes/editor.js
@@ -7,26 +7,29 @@ const process = require("process");
 
 router.get("/*", function (req, res, next) {
 
-
-  res.render("editor", {
-    content: contents
-  });
-  //---------
-  console.log("read the contents of " + req.originalUrl)
-  console.log("process.cwd() " + process.cwd());
-  path = process.cwd() + req.originalUrl.replace("/" + constants.RESOURCE, "/Notes")
-  console.log('path ' + path)
-  if (path.includes("/images")) {
-    // fs.readFile(path, 'utf8', function (err, contents) {
-    res.sendFile(path);
-    //});
-  } else {
-    fs.readFile(path, 'utf8', function (err, contents) {
-      res.render("resource", {
-        content: contents
-      });
+  let path = "../content/template/editor.html"
+  fs.readFile(path, 'utf8', function (err, contents) {
+    res.render("resource", {
+      content: contents
     });
-  }
+  });
+
+  //---------
+  // console.log("read the contents of " + req.originalUrl)
+  // console.log("process.cwd() " + process.cwd());
+  // path = process.cwd() + req.originalUrl.replace("/" + constants.RESOURCE, "/Notes")
+  // console.log('path ' + path)
+  // if (path.includes("/images")) {
+  //   // fs.readFile(path, 'utf8', function (err, contents) {
+  //   res.sendFile(path);
+  //   //});
+  // } else {
+  //   fs.readFile(path, 'utf8', function (err, contents) {
+  //     res.render("resource", {
+  //       content: contents
+  //     });
+  //   });
+  // }
 });
 
 
